44a40bb41ef941f29bba8f7c63f2ec0f74ea3cc8
Mango-Juice <wjsalsrb5@naver.com>
2022-05-02 01:47:32 +0900

    feat : 다이얼 - Appointment 연동 함수 추가
    Dial을 Appointment로 형변환하여 사용할 수 있도록 수정.
    DialManager에 다이얼 추가 함수, 오늘의 다이얼을 불러오는 함수 추가
    클래스 이름 수정 및 테스트 파일 추가

---
+++ lib/models/schedule.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+import 'package:plan_dial_renewal/models/time.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+/// 시작일, 종료일을 담는 클래스
+class Schedule {
+  static const defaultDuration = 1;
+  final Time start;
+  late final Time finish;
+
+  Schedule(this.start, [Time? finish]) {
+    if (finish == null) {
+      this.finish = Time(start.hour + defaultDuration, start.minute);
+    } else {
+      assert(finish.isLaterThan(start)); // start가 finish 이전이어야 함.
+      this.finish = finish;
+    }
+  }
+
+  Appointment toAppointment(String title, Color color, DateTime date) {
+    return Appointment(
+      startTime: start.toDateTime(date.year, date.month, date.day),
+      endTime: finish.toDateTime(date.year, date.month, date.day),
+      subject: title,
+      color: color,
+    );
+  }
+}
